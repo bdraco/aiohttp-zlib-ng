56d4b883bc1a1344e120f31c477cda35cb5aa9b9
J. Nick Koston <nick@koston.org>
2023-11-02 09:34:12 -0500

    feat: add first version

--- src/aiohttp_zlib_ng/__init__.py
+++ src/aiohttp_zlib_ng/__init__.py
@@ -18,35 +18,19 @@ TARGETS = (
 
 def enable_zlib_ng() -> None:
     """Enable zlib-ng."""
-    for location in (
-        "compression_utils",
-        "http_writer",
-        "http_websocket",
-        "http_writer",
-        "multipart",
-        "web_response",
-    ):
+    for location in TARGETS:
         try:
             importlib.import_module(f"aiohttp.{location}")
         except ImportError:
             continue
-        module = getattr(aiohttp, location, None)
-        if module:
+        if module := getattr(aiohttp, location, None):
             module.zlib = zlib_ng
 
 
 def disable_zlib_ng() -> None:
     """Disable zlib-ng and restore the original zlib."""
-    for location in (
-        "compression_utils",
-        "http_writer",
-        "http_websocket",
-        "http_writer",
-        "multipart",
-        "web_response",
-    ):
-        module = getattr(aiohttp, location, None)
-        if module:
+    for location in TARGETS:
+        if module := getattr(aiohttp, location, None):
             module.zlib = zlib_original
 
 
