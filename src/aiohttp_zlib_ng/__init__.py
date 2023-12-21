f841ceee40b7f23a97e38978af878ae499163ad3
J. Nick Koston <nick@koston.org>
2023-12-21 08:12:56 -1000

    fix: disable library if AVX is missing (#4)

--- src/aiohttp_zlib_ng/__init__.py
+++ src/aiohttp_zlib_ng/__init__.py
@@ -1,10 +1,15 @@
 __version__ = "0.0.0"
 
 import importlib
+import logging
+import platform
 import zlib as zlib_original
+from typing import Any, Dict, Optional
 
 import aiohttp
 from zlib_ng import zlib_ng as zlib_ng
+
+_LOGGER = logging.getLogger(__name__)
 
 TARGETS = (
     "compression_utils",
@@ -16,9 +21,30 @@ TARGETS = (
     "web_response",
 )
 
+CPUFeature: Optional[Dict[str, Any]] = None
+
+if platform.machine() == "x86_64":
+    try:
+        from cpufeature import CPUFeature  # type: ignore[no-redef]
+    except ImportError:
+        pass
+
+HAS_MISSING_AVX = bool(CPUFeature and not CPUFeature.get("AVX"))
+# HAS_MISSING_AVX is True if AVX is not supported and using x86_64.
+#
+# This is a workaround to disable zlib-ng on x86_64 if AVX is not supported
+# on older CPUs until https://github.com/pycompression/python-zlib-ng/pull/15
+# is merged and released.
+#
+# See https://github.com/home-assistant/core/issues/105254
+#
+
 
 def enable_zlib_ng() -> None:
     """Enable zlib-ng."""
+    if HAS_MISSING_AVX:
+        _LOGGER.debug("AVX is not supported, disabling zlib-ng")
+        return
     for location in TARGETS:
         try:
             importlib.import_module(f"aiohttp.{location}")
@@ -30,6 +56,8 @@ def enable_zlib_ng() -> None:
 
 def disable_zlib_ng() -> None:
     """Disable zlib-ng and restore the original zlib."""
+    if HAS_MISSING_AVX:
+        return
     for location in TARGETS:
         if module := getattr(aiohttp, location, None):
             module.zlib = zlib_original
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,8 +1,10 @@
 import zlib as zlib_original
+from unittest.mock import patch
 
 import aiohttp.http_websocket
 from zlib_ng import zlib_ng as zlib_ng
 
+import aiohttp_zlib_ng
 from aiohttp_zlib_ng import disable_zlib_ng, enable_zlib_ng
 
 
@@ -15,3 +17,16 @@ def test_enable_disable():
     assert aiohttp.http_websocket.zlib is zlib_original
     enable_zlib_ng()
     assert aiohttp.http_websocket.zlib is zlib_ng
+    disable_zlib_ng()
+
+
+def test_disabled_if_no_avx():
+    """Test disabled if no AVX."""
+    assert aiohttp.http_websocket.zlib is zlib_original
+    with patch.object(aiohttp_zlib_ng, "HAS_MISSING_AVX", True):
+        enable_zlib_ng()
+        assert aiohttp.http_websocket.zlib is zlib_original
+        disable_zlib_ng()
+        assert aiohttp.http_websocket.zlib is zlib_original
+        enable_zlib_ng()
+        assert aiohttp.http_websocket.zlib is zlib_original
