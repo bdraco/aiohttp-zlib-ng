ebe8353221d8dca0ba03400b39b06a15b614d81a
J. Nick Koston <nick@koston.org>
2023-12-27 07:03:12 -1000

    fix: revert disable if no avx check in favor of new zlib_ng (#5)

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,10 +1,8 @@
 import zlib as zlib_original
-from unittest.mock import patch
 
 import aiohttp.http_websocket
 from zlib_ng import zlib_ng as zlib_ng
 
-import aiohttp_zlib_ng
 from aiohttp_zlib_ng import disable_zlib_ng, enable_zlib_ng
 
 
@@ -18,15 +16,3 @@ def test_enable_disable():
     enable_zlib_ng()
     assert aiohttp.http_websocket.zlib is zlib_ng
     disable_zlib_ng()
-
-
-def test_disabled_if_no_avx():
-    """Test disabled if no AVX."""
-    assert aiohttp.http_websocket.zlib is zlib_original
-    with patch.object(aiohttp_zlib_ng, "HAS_MISSING_AVX", True):
-        enable_zlib_ng()
-        assert aiohttp.http_websocket.zlib is zlib_original
-        disable_zlib_ng()
-        assert aiohttp.http_websocket.zlib is zlib_original
-        enable_zlib_ng()
-        assert aiohttp.http_websocket.zlib is zlib_original
